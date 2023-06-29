71797070d943a02fc75162bfea1a028891b8d169
3D-uy <136130959+3D-uy@users.noreply.github.com>
2023-06-29 19:00:38 -0300

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -1,75 +1,73 @@
 /**
- * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Firmware de Impresora 3D Marlin
+ * Derechos de autor (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
- * Based on Sprinter and grbl.
- * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ * Basado en Sprinter y grbl.
+ * Derechos de autor (c) 2011 Camiel Gubbels / Erik van der Zalm
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Este programa es software libre: puede redistribuirlo y/o modificarlo
+ * bajo los términos de la Licencia Pública General de GNU publicada por
+ * la Free Software Foundation, ya sea la versión 3 de la Licencia, o
+ * (a su elección) cualquier versión posterior.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Este programa se distribuye con la esperanza de que sea útil,
+ * pero SIN NINGUNA GARANTÍA; sin siquiera la garantía implícita de
+ * COMERCIABILIDAD o APTITUD PARA UN PROPÓSITO PARTICULAR. Consulte la
+ * Licencia Pública General de GNU para obtener más detalles.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Debería haber recibido una copia de la Licencia Pública General de GNU
+ * junto con este programa. Si no lo hizo, consulte <https://www.gnu.org/licenses/>.
  *
  */
 #pragma once
 
-////////////////////////////
-// VENDOR VERSION EXAMPLE //
-////////////////////////////
+/////////////////////////////////////
+// EJEMPLO DE VERSIÓN DEL VENDEDOR //
+/////////////////////////////////////
 
 /**
- * Marlin release version identifier
+ * Identificador de versión de lanzamiento de Marlin
  */
 //#define SHORT_BUILD_VERSION "bugfix-2.1.x"
 
 /**
- * Verbose version identifier which should contain a reference to the location
- * from where the binary was downloaded or the source code was compiled.
+ * Identificador de versión detallado que debe contener una referencia al lugar
+ * desde donde se descargó el archivo binario o se compiló el código fuente.
  */
 //#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
 
 /**
- * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
- * here we define this default string as the date where the latest release
- * version was tagged.
+ * La cadena STRING_DISTRIBUTION_DATE representa la fecha en que se construyó el archivo binario,
+ * aquí definimos esta cadena predeterminada como la fecha en la que se etiquetó la última versión de lanzamiento.
  */
-//#define STRING_DISTRIBUTION_DATE "2023-06-29"
+//#define STRING_DISTRIBUTION_DATE "2023-06-06"
 
 /**
- * Defines a generic printer name to be output to the LCD after booting Marlin.
+ * Define un nombre genérico de impresora que se mostrará en la pantalla LCD después de arrancar Marlin.
  */
-//#define MACHINE_NAME "3D Printer"
+//#define MACHINE_NAME "Impresora 3D"
 
 /**
- * The SOURCE_CODE_URL is the location where users will find the Marlin Source
- * Code which is installed on the device. In most cases —unless the manufacturer
- * has a distinct Github fork— the Source Code URL should just be the main
- * Marlin repository.
+ * La URL del CÓDIGO FUENTE es el lugar donde los usuarios encontrarán el Código Fuente de Marlin
+ * que está instalado en el dispositivo. En la mayoría de los casos, a menos que el fabricante
+ * tenga un repositorio de Github distinto, la URL del Código Fuente debe ser simplemente el repositorio principal de Marlin.
  */
 //#define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
 
 /**
- * Default generic printer UUID.
+ * UUID genérico de la impresora predeterminada.
  */
 //#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
 
 /**
- * The WEBSITE_URL is the location where users can get more information such as
- * documentation about a specific Marlin release.
+ * La URL del SITIO WEB es el lugar donde los usuarios pueden obtener más información, como
+ * documentación sobre una versión específica de Marlin.
  */
 //#define WEBSITE_URL "marlinfw.org"
 
 /**
- * Set the vendor info the serial USB interface, if changable
- * Currently only supported by DUE platform
+ * Establece la información del vendedor en la interfaz USB serie, si es modificable.
+ * Actualmente solo compatible con la plataforma DUE.
  */
 //#define USB_DEVICE_VENDOR_ID           0x0000
 //#define USB_DEVICE_PRODUCT_ID          0x0000
